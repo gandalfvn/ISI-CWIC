f99bc3727d899bc11bc2eb22deeddba7f09ffd06
William Wong <william@greenratio.com>
2015-07-27 16:10:32 -0700

    added collision detection and some shaders

--- bwapp/client/angle/js/modules/controllers/world-view.js
+++ bwapp/client/angle/js/modules/controllers/world-view.js
@@ -10,35 +10,35 @@ angular.module('angle').controller('worldCtrl',
 
      // Get the canvas element from our HTML above
      var canvas = document.getElementById("renderCanvasBab");
-
      // Load the BABYLON 3D engine
      var engine = new BABYLON.Engine(canvas, true);
 
 // This begins the creation of a function that we will 'call' just after it's built
      var createScene = function () {
-
        // Now create a basic Babylon Scene object
        var scene = new BABYLON.Scene(engine);
        scene.enablePhysics(new BABYLON.Vector3(0, -10, 0), new BABYLON.OimoJSPlugin());
-
        // Change the scene background color to green.
        scene.clearColor = new BABYLON.Color3(0, 0, 0.5);
+       scene.collisionsEnabled = true;
 
        // This creates and positions a free camera
        var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -30), scene);
-
        // This targets the camera to scene origin
        camera.setTarget(BABYLON.Vector3.Zero());
-
        // This attaches the camera to the canvas
-       camera.attachControl(canvas, true);
-       camera.speed = 2;
+       //camera.attachControl(canvas, true);
+       camera.speed = 1;
+       camera.ellipsoid = new BABYLON.Vector3(1, 1, 1); //bounding ellipse
+       camera.checkCollisions = true;
+
+       scene.activeCamera = camera;
+       scene.activeCamera.attachControl(canvas, true);
 
        // This creates a light, aiming 0,1,0 - to the sky.
        var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-
        // Dim the light a small amount
-       light.intensity = .5;
+       light.intensity = 0.9;
 
        var boxmat = new BABYLON.StandardMaterial("cube1", scene);
        var boxt = new BABYLON.Texture("img/texture/wood.jpg", scene);
@@ -50,7 +50,8 @@ angular.module('angle').controller('worldCtrl',
        box.material = boxmat;
        // Move the box upward 1/2 its height
        box.position.y = 5;
-       box.setPhysicsState({impostor:BABYLON.PhysicsEngine.BoxImpostor, move:true, mass:1, friction:0.5, restitution:0.1});
+       box.setPhysicsState({impostor:BABYLON.PhysicsEngine.BoxImpostor, move:true, mass:4, friction:0.5, restitution:0.1});
+       box.checkCollisions = true;
 
        /** SKYBOX **/
        BABYLON.Engine.ShadersRepository = "shaders/";
@@ -68,17 +69,20 @@ angular.module('angle').controller('worldCtrl',
        /** GROUND **/
        // Material
        var mat = new BABYLON.StandardMaterial("ground", scene);
-       var t = new BABYLON.Texture("img/texture/mixgrass.jpg", scene);
+       var t = new BABYLON.Texture("img/texture/plasticwhite.jpg", scene);
        t.uScale = t.vScale = 10;
        mat.diffuseTexture = t;
        mat.specularColor = BABYLON.Color3.Black();
+       var gridshader = new BABYLON.ShaderMaterial("grid", scene, "grid", {});
+
        // Object
        var g = BABYLON.Mesh.CreateBox("ground", 100, scene);
        g.position.y = -10;
        g.scaling.y = 0.01;
 
-       g.material = mat;
+       g.material = mat; //gridshader; //mat;
        g.setPhysicsState({ impostor: BABYLON.PhysicsEngine.BoxImpostor, move:false});
+       g.checkCollisions = true;
 
        // Leave this function
        return scene;
