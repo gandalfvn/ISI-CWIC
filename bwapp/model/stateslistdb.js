55f36a9d7d6970d935ec7ac77c6fce7209abe6ac
William Wong <william@greenratio.com>
2015-10-03 21:34:27 -0700

    remove stateslist in server code

--- bwapp/model/stateslistdb.js
+++
@@ -1,6 +0,0 @@
-/**========================================================
- * Module: stateslistdb.js
- * Created by wjwong on 9/16/15.
- =========================================================*/
-
-StatesList = new Mongo.Collection('stateslist');
